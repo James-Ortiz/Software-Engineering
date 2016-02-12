2fae9364dd47e1949e953b8472f080d5b7b3a706
DukeLimousine <didomenico@students.rowan.edu>
2016-02-12 15:01:47 -0500

    Doin work on the reader. Created Flight class.

---
+++ Java/src/Flight.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+/**
+ * Created by HansYolo on 2/12/2016.
+ */
+public class Flight {
+
+    private HashMap flightData;
+
+    public Flight(HashMap attributes){
+
+        flightData = attributes;
+
+    }
+}
